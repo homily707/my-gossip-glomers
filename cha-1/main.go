cc2b71bf41a9e22f300cf96184d9301b1e89cf26
homily707 <homily707@outlook.com>
2023-12-19 15:25:41 +0800

    Finish cha-1

---
+++ cha-1/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func main() {
+	n := maelstrom.NewNode()
+	n.Handle("echo", func(msg maelstrom.Message) error {
+		var body map[string]interface{}
+		if err := json.Unmarshal(msg.Body, body); err != nil {
+			return err
+		}
+		body["type"] = "echo_ok"
+		msg.Src, msg.Dest = msg.Dest, msg.Src
+		return n.Reply(msg, body)
+	})
+
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/homily707/my-gossip-glomers
+
+go 1.21.0
+
+require github.com/jepsen-io/maelstrom/demo/go v0.0.0-20231205140322-b59de21565d8
